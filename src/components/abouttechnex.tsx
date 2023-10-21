a1c94beaea9bf7364bd3824c3b1164dc15a1ed11
Vyas Giri <vyasgiri25@gmail.com>
2023-10-22 00:59:13 +0530

    Made hamburger menu and hover effects

--- src/components/abouttechnex.tsx
+++ src/components/abouttechnex.tsx
@@ -8,13 +8,13 @@ const AboutTechnex: FC = () => {
             <div className='flex items-center justify-center pt-28 animate-bounce'>
             <ArrowRightIcon className='rotate-90' boxSize={32}/>
             </div>
-            <div className=' ml-5 sm:ml-10 md:ml-22 lg:ml-28 xl:ml-40 -space-y-10 sm:-space-y-20 md:-space-y-16 lg:-space-y-24'>
+            <div className='ml-1 sm:ml-10 md:ml-22 lg:ml-28 xl:ml-40 -space-y-10 sm:-space-y-20 md:-space-y-16 lg:-space-y-24'>
                 <div className='-z-10'>
-                <span className='text-[#A81F25] opacity-30 text-7xl sm:text-[10rem] lg:text-[14rem] xl:text-[16rem] font-bold'>Technex</span>
+                <span className='text-[#A81F25] opacity-30 text-6xl sm:text-[10rem] lg:text-[14rem] xl:text-[16rem] font-bold'>Technex</span>
                 </div>
-                <div className='z-0 pb-10 ml-5 sm:ml-8 lg:ml-12 xl:ml-14'>
-                    <span className='text-3xl sm:text-6xl md:text-5xl lg:text-7xl font-bold'>About <span className='text-[#A81F25]'>Technex</span></span>
-                    <p className='pt-10 lg:w-[850px] tracking-wide p-3 pr-5'>TECHNEX&apos;23, IIT BHU is one of the largest and oldest college fests in India.&nbsp; Embodying the true spirit of youth, <span className='text-[#A81F25]'>Technex provides a platform for young talent from all over India to showcase their varied skills.</span>&nbsp;
+                <div className='z-0 pb-10 ml-3 sm:ml-8 lg:ml-12 xl:ml-14'>
+                    <span className='text-3xl sm:text-6xl md:text-5xl lg:text-7xl font-extrabold'>About <span className='text-[#A81F25]'>Technex</span></span>
+                    <p className='pt-10 lg:w-[850px] tracking-wide p-3 pr-5 border-b-2 border-[#A81F25] mr-3'>TECHNEX&apos;23, IIT BHU is one of the largest and oldest college fests in India.&nbsp; Embodying the true spirit of youth, <span className='text-[#A81F25]'>Technex provides a platform for young talent from all over India to showcase their varied skills.</span>&nbsp;
                     Technex is an annual technical festival organized by the students of Indian Institute of Technology (IIT) BHU,&nbsp; Varanasi.<br></br>
                     The festival provides a platform for students to showcase their technical and creative abilities through <span className='text-[#A81F25]'>various competitions, workshops, guest lectures, and exhibitions</span>. It attracts a large number of students from all over the country and provides a unique opportunity for them to showcase their skills and network with their peers and industry professionals. <span className='text-[#A81F25]'>Technex is a celebration of innovation and technology, and a platform for students to learn, grow and excel in their field of interest.</span></p>
                 </div>
--- src/components/landingpage.tsx
+++ src/components/landingpage.tsx
@@ -1,17 +1,18 @@
 import React, { FC } from 'react'
 import Navbar from './navbar'
 import AboutTechnex from './abouttechnex'
+import Link from 'next/link'
 
 const Landingpage: FC = () => {
   return (
     <div className='scroll-smooth'>
-        <div className="bg-[url('/bg_landing_page.webp')] bg-no-repeat bg-cover pb-48">
-            <div className='pb-96'>
+        <div className="bg-[url('/bg_landing_page.webp')] bg-no-repeat bg-cover pb-4 xl:pb-16  2xl:pb-48">
+            <div className='pb-96 lg:pb-64 xl:pb-96 z-10'>
                 <Navbar/>
             </div>
-            <div className='ml-5 sm:ml-10 md:ml-20 lg:ml-40'>
-                <h1 className='font-bold text-5xl sm:text-7xl pb-5'>Become Our <span className='text-[#A81F25] font-bold text-5xl sm:text-7xl pb-3'>Campus Ambassador</span></h1>
-                <div className='text-lg font-normal'><p className='border-2 py-1 px-5 rounded-e-full rounded-s-full w-28'>Register</p></div>
+            <div className='ml-2 sm:ml-10 md:ml-20 lg:ml-40'>
+                <h1 className='font-bold text-3xl sm:text-6xl pb-1 sm:pb-5'>Become Our <span className='text-[#A81F25] font-bold text-3xl sm:text-6xl'>Campus Ambassador</span></h1>
+                <div className='text-lg font-normal'><Link href={"/register"} className='border-2 hover:border-[#A81F25] hover:text-[#A81F25] hover:bg-[#191919] py-1 px-5 rounded-e-full rounded-s-full w-28'>Register</Link></div>
             </div>
         </div>
         <AboutTechnex />
