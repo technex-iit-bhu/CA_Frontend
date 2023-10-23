63ce3638d8e30dcdb324fbf801abe8b3593cc69e
swetagit186 <sweta.kumari.eee22@itbhu.ac.in>
2023-10-23 18:18:06 +0530

    mase the button

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,56 +1,49 @@
 import React from 'react'
-import Image from 'next/image'
 
-export default function profilet() {
-    return (
-    <div className='flex flex-col p-20  m-20 mb-10 '>
-      <div className='mb-44'>
-        <div className=' text-7xl  left-[130px]'>Profile</div>
-      </div>
-      <div className='bg-grey m-6 lg:w-[1129px] rounded-3xl'>
-      <div className='flex p-20 flex-wrap items-center'>
-        {/* left */}
-        <div className='w-[300px] relative'>
-          <div className=''>
-            <Image src="/profile-user.webp" alt='image not found' height={230} width={230} className=' border-[5px] border-red rounded-full absolute left-0 -top-[200px]'/>
-          </div>
-          <div className='mt-[100px]'>
-          <h1 className='text-5xl mb-4 flex flex-wrap '>AmuseBoucheSharma</h1>
-          <p className=''>Hello I am a keen enthusiast in Machine Learning, Meko IG mein lelo plz I do bohot saara machine learning with my machine. </p>
+
+export default function profile ()  {
+  return (
+    <div>
+        <div className='flex flex-col bg-background pt-[200px] relative pl-[120px] md:mb-[50px] mb-[100px]'>
+          <div className='flex text-5xl absolute align-left left-5 lg:text-7xl lg:p-10 md:p-10 bottom-[10px] sm:text-center'>
+            <span className='text-white select-none'>Profile</span>
           </div>
         </div>
-        {/* middle line */}
-        <div className='h-[400px] w-[1px] sm:hidden md:hidden lg:block  bg-red mr-8 ml-44 justify-center'></div>
-        {/* <div className='mt mb pl-8 pr-8 hidden sm:block '>
-        
-        <Image 
-        src={"/Line2.webp"}
-        alt='line:)'
-        width={1}
-        height={100}
-        className=''/>
-        </div> 
-         */}
-        <div className='flex flex-col justify-center gap-8  '>
-          <div className='flex lg:flex-row flex-col flex-wrap gap-8 bg-black2 lg:p-4 p-2 rounded-full'>
-            <h2>College:</h2>
-            <p>IIT BHU, they hate your guts</p>
-          </div>
-          <div className='flex gap-8 bg-black2 p-4 rounded-full'>
-          <h2>Email-id:</h2>
-            <p>random.random.mat22@itbhu.ac.in</p>
-          </div>
-          <div className='flex gap-8 bg-black2 p-4 rounded-full'>
-          <h2>Phone No:</h2>
-            <p>9004189582</p>
-          </div>
-          <div className='flex gap-8 bg-black2 p-4 rounded-full'>
-            <h2>Address:</h2>
-            <p>At home, IITB, Powai, Bombay</p>
-          </div>
+        <div className='px-[30px] md:px-[50px] '>
+                <div className='bg-grey px-[50px] md:px-[30px] py-[30px] rounded-[50px] mb-20 z-0'>
+                    <div className='flex lg:flex-row flex-col  lg:pl-[100px]'>
+                        <div className='flex flex-col mt-[-80px]'>
+                            <div className='select-none bg-background w-[300px] h-[300px] rounded-full border-4 border-red lg:self-start self-center'></div>
+                            <div className='flex flex-col lg:pl-[20px] lg:pr-[60px] pt-[20px] pb-[30px] lg:w-[500px]'>
+                                <p className='select-none lg:self-start text-white font-bold text-[60px] self-center flex flex-wrap lg:text-left text-center'>Amuse Bouche Sharma</p>
+                                <p className='select-none lg:self-start text-white text-[25px] self-center flex flex-wrap'>Hello I am a keen enthusiast in Machine Learning, Meko IG mein lelo plz
+                                I do bohot saara machine learning with my machine. </p>
+                            </div>
+                        </div>
+                        <div className='lg:h-[800px] lg:w-[2px] h-[2px] w-auto bg-red'></div>
+                        <div className='flex flex-col lg:pl-[100px] lg:pr-[100px]  lg:w-[400px] justify-center  self-center grow pt-[50px] gap-20 '>
+                            <div className='  lg:h-[80px] lg:w-[500px]   md:rounded-[50px] bg-black2 p-4 rounded-[25px] flex flex-row   mr-[10px] gap-[20px]'>
+                                <p className='select-none text-white font-bold sm:text-[25px] text-[10px] self-center '>College:</p>
+                                <p className='select-none text-white sm:text-[25px] text-[10px] self-center   '>IIT BHU, they hate your guts</p>
+                            </div>
+                            <div className='  lg:h-[80px] lg:w-[500px]   md:rounded-[50px] bg-black2 p-4 rounded-[25px] flex flex-row   mr-[10px] gap-[20px]'>
+                                <p className='select-none text-white font-bold sm:text-[25px] text-[10px] self-center '>Email-id:</p>
+                                <p className='select-none text-white sm:text-[25px] text-[10px] self-center flex-wrap  '>random.mat 22@itbhu.ac.in</p>
+                            </div>
+                            
+                            <div className='lg:h-[80px] lg:w-[500px]   md:rounded-[50px] bg-black2 p-4 rounded-[25px] flex flex-row   mr-[10px] gap-[20px]' >
+                                <p className='select-none text-white font-bold sm:text-[25px] text-[10px] self-center  '>Phone No:</p>
+                                <p className='select-none text-white  sm:text-[25px] text-[10px] self-center'>9004189582</p>
+                            </div>
+                            <div className='  lg:h-[80px] lg:w-[500px]   md:rounded-[50px] bg-black2 p-4 rounded-[25px] flex flex-row   mr-[10px] gap-[20px]' >
+                                <p className='select-none text-white font-bold sm:text-[25px] text-[10px] self-center  '>Address: </p>
+                                <p className='select-none text-white  sm:text-[25px] text-[10px] self-center'>At home, IITB, Powai, Bombay</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>    
         </div>
-      </div>
-      </div>
-    </div>)
-  }
-  
+        <div className='flex items-center justify-center lg:p-6 md:p-6 p-6 bg-red lg:w-[393px] md:w-[200px] w-[100px] mx-auto mb-[100px] rounded-full '><button className='lg:text-4xl sm:text-xl'>Dashboard</button></div>
+    </div>
+  )
+}
