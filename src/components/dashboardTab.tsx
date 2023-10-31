40e8b88681c7ee57ca24151aeb5c598895304431
ashutuptiwari <ashutosh.tiwari.cd.ece22@itbhu.ac.in>
2023-10-31 13:13:43 +0530

    add props , add links to social media

--- src/components/dashboardTab.tsx
+++ src/components/dashboardTab.tsx
@@ -9,8 +9,8 @@ const DashboardTab = () => {
     setActiveTab(tabNumber);
   };
   const [isLargerThan700] = useMediaQuery('(min-width: 700px)');
-  const fontSize= isLargerThan700?'20px':'3.57vw';
-  const width = isLargerThan700?'200px':'28.5vw';
+  const fontSize = isLargerThan700 ? '20px' : '3.57vw';
+  const width = isLargerThan700 ? '200px' : '28.5vw';
 
   return (
     <Box>
@@ -18,7 +18,7 @@ const DashboardTab = () => {
         <Button
           fontSize={fontSize}
           w={width}
-          borderBottom={activeTab === 1 ? '0.5vw solid #A81F25' : 'none'}
+          borderBottom={activeTab === 1 ? '0.5vh solid #A81F25' : 'none'}
           onClick={() => handleTabClick(1)}
         >
           Live Tasks
@@ -26,7 +26,7 @@ const DashboardTab = () => {
         <Button
           fontSize={fontSize}
           w={width}
-          borderBottom={activeTab === 2 ? '0.5vw solid #A81F25' : 'none'}
+          borderBottom={activeTab === 2 ? '0.5vh solid #A81F25' : 'none'}
           onClick={() => handleTabClick(2)}
         >
           Completed
@@ -34,7 +34,7 @@ const DashboardTab = () => {
         <Button
           fontSize={fontSize}
           w={width}
-          borderBottom={activeTab === 3 ? '0.5vw solid #A81F25' : 'none'}
+          borderBottom={activeTab === 3 ? '0.5vh solid #A81F25' : 'none'}
           onClick={() => handleTabClick(3)}
         >
           Expired
@@ -42,20 +42,62 @@ const DashboardTab = () => {
       </Box>
       {activeTab === 1 && (
         <Box>
-          <Cards />
+          <Cards
+            date='01/06/24'
+            points='100'
+            heading='Connect To Technex'
+            text='Connect to Technex and find your soulmate. Try and get laid in India’s biggest Techno-Management Fest'
+            taskNumber='1'
+            month='June'
+          />
         </Box>
       )}
       {activeTab === 2 && (
         <Box>
-          <Cards />
-          <Cards />
+          <Cards
+            date='01/06/24'
+            points='100'
+            heading='Connect To Technex'
+            text='Connect to Technex and find your soulmate. Try and get laid in India’s biggest Techno-Management Fest'
+            taskNumber='1'
+            month='June'
+          />
+          <Cards
+            date='08/06/24'
+            points='150'
+            heading='Connect To Technex'
+            text='Connect to Technex and find your soulmate. Try and get laid in India’s biggest Techno-Management Fest'
+            taskNumber='2'
+            month='June'
+          />
         </Box>
       )}
       {activeTab === 3 && (
         <Box>
-          <Cards />
-          <Cards />
-          <Cards />
+          <Cards
+            date='01/05/24'
+            points='100'
+            heading='Connect To Technex'
+            text='Connect to Technex and find your soulmate. Try and get laid in India’s biggest Techno-Management Fest'
+            taskNumber='1'
+            month='May'
+          />
+          <Cards
+            date='01/05/24'
+            points='50'
+            heading='Connect To Technex'
+            text='Connect to Technex and find your soulmate. Try and get laid in India’s biggest Techno-Management Fest'
+            taskNumber='2'
+            month='May'
+          />
+          <Cards
+            date='69/69/24'
+            points='-69'
+            heading='Connect To Technex'
+            text='Connect to Technex and find your soulmate. Try and get laid in India’s biggest Techno-Management Fest'
+            taskNumber='3'
+            month='May'
+          />
         </Box>
       )}
     </Box>
