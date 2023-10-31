7a9d97d82312cc49928ba62a3e89747144320d12
Vyas Giri <vyasgiri25@gmail.com>
2023-10-31 19:03:25 +0530

    Fixed abouttechnex and date

--- src/components/landingpage.tsx
+++ src/components/landingpage.tsx
@@ -6,12 +6,12 @@ import Link from 'next/link';
 const Landingpage: FC = () => {
   return (
     <div className='scroll-smooth'>
-      <div className="bg-[url('/bg_landing_page.webp')] bg-cover mb-5 max-w-full h-auto bg-fixed bg-center bg-no-repeat pb-8 xl:pb-16 2xl:pb-48">
-        <div className='z-10 pb-96 sm:pb-80 lg:pb-64 xl:pb-96'>
+      <div className="bg-[url('/bg_landing_page.webp')] bg-cover mb-5 max-w-full h-auto bg-fixed bg-center bg-no-repeat pb-8 xl:pb-16 2xl:pb-48 -space-y-20 sm:-space-y-0">
+        <div className='z-10 pb-[31rem] sm:pb-80 lg:pb-64 xl:pb-96'>
           <Navbar />
         </div>
-        <div className='ml-2 sm:ml-10 md:ml-20 lg:ml-40 ' >
-          <h1 className='pb-1 text-3xl font-bold sm:pb-5 sm:text-6xl font-spline '>
+        <div className='ml-2 sm:ml-10 md:ml-20 lg:ml-40 mb-5'>
+          <h1 className='pb-1 text-3xl font-bold sm:pb-5 sm:text-6xl font-spline'>
             Become Our{' '}
             <span className='text-3xl font-bold text-[#A81F25] sm:text-6xl'>
               Campus Ambassador
