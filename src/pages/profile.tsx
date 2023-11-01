9cf3eef48480412ff65b625b22233c215fe174f7
swetagit186 <sweta.kumari.eee22@itbhu.ac.in>
2023-11-01 19:15:00 +0530

    making responsive

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -3,14 +3,20 @@ import React from 'react';
 export default function profile() {
   return (
     <div>
-      <div className='relative mb-[160px] flex flex-col bg-background lg:pl-[68px] pl-[50px] pt-[250px] md:mb-[70px] lg:pb-[150px] md:pb-10'>
-        <div className=' absolute bottom-[10px] left-0 flex text-5xl md:p-10 lg:p-[90px] lg:text-7xl'>
+      <div className='relative mb-[160px] flex flex-col bg-background lg:pl-[68px] pl-[50px] pt-[80px] md:mb-[70px] lg:pb-[100px] md:pb-10'>
+        {/* <div className=' absolute bottom-[10px] left-0 flex text-5xl md:p-10 lg:p-[90px] lg:text-7xl'>
           <div className='flex flex-col sm:pb-10 -space-y-9 sm:-space-y-16 md:-space-y-16 lg:-space-y-20 xl:-space-y-24'>
-            <span className='text-6xl lg:pl-3 md:pl-5  cd:pl-5 ef:pl-8 gh:pl-12  pl-9 font-bold text-[#A81F25] opacity-20 sm:text-[5rem] lg:text-[7rem] xl:text-[18.5rem] font-spline'>
+            <span className='text-6xl xl:pl-10 lg:pl-3 md:pl-5  cd:pl-5 ef:pl-8 gh:pl-12  pl-9 font-bold text-[#A81F25] opacity-20 sm:text-[5rem] lg:text-[7rem] xl:text-[18rem] font-spline'>
             Profile
             </span>
-            <span className='lg:p-0 md:pl-5 ef:pl-5 gh:pl-6 pl-6  text-white select-none ml-4 sm:ml-8 text-2xl sm:text-[60px] lg:text-[82px] z-10 font-spline'>Profile</span>
+            <span className='lg:p-0 md:pl-5 ef:pl-5 gh:pl-6 pl-6  text-white select-none ml-4 sm:ml-8 text-2xl sm:text-[60px] lg:text-[82px]  z-10 font-spline'>Profile</span>
           </div>
+        </div> */}
+        <div className='flex flex-col sm:pb-10 -space-y-9 sm:-space-y-16 md:-space-y-16 lg:-space-y-20 xl:-space-y-24'>
+          <span className='text-6xl xl:pl-6 font-bold text-[#A81F25] opacity-20 sm:text-[10rem] lg:text-[7rem] xl:text-[9rem] font-spline'>
+            Profile
+          </span>
+          <span className='sm:pl-1 lg:pl-0 xl:pl-0 xl:pt-10 text-white select-none ml-4 sm:ml-8 text-2xl sm:text-[30px] lg:text-[41px] z-12'>Profile</span>
         </div>
       </div>
       <div className='px-[30px] md:px-[50px] '>
