8c95141e773be6f46b9611907303367303de6356
Kislay <88523649+kuv2707@users.noreply.github.com>
2023-11-08 00:08:02 +0530

    [ADDED] time interval since login and bug fixes

--- src/components/AdminPage/TaskList.tsx
+++ src/components/AdminPage/TaskList.tsx
@@ -10,10 +10,10 @@ type Task = {
 const BACKEND_URL = 'http://localhost:8000/'; //TODO: move to .env
 function TaskList({ token }: { token: string | null }) {
   const [tasks, setTasks] = useState<Task[]>([]);
-    const [loading, setLoading] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
   const fetchTasks = async () => {
     try {
-        setLoading(true);
+      setLoading(true);
       const response = await fetch(BACKEND_URL + 'tasks/', {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -24,26 +24,22 @@ function TaskList({ token }: { token: string | null }) {
     } catch (error) {
       console.error('Error fetching tasks:', error);
       setTasks([]);
-    } finally{
-        setLoading(false);
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     fetchTasks();
-  }, []); // Empty dependency array ensures the effect runs once after the initial render
+  }, []);
 
   const handleRefresh = () => {
     fetchTasks();
   };
 
-  function handleDeleteTask(id: number) {
-    //todo make fetch request
-    handleRefresh();
-  }
-
   return (
     <div>
+        <h2>Click on update button after making changes to a task.</h2>
       <table>
         <thead>
           <tr>
@@ -55,33 +51,88 @@ function TaskList({ token }: { token: string | null }) {
         </thead>
         <tbody>
           {tasks.map((task) => (
-            <tr key={task.id}>
-              <td className={styles.td}>{task.id}</td>
-              <td className={styles.td}>{task.title}</td>
-              <td className={styles.td}>
-                <textarea className={styles.textarea} value={task.description} contentEditable={false}>
-                </textarea>
-              </td>
-              <td className={styles.td}>{task.points}</td>
-              <td className={styles.td}>
-                <button
-                  className={styless.button}
-                  onClick={() => handleDeleteTask(task.id)}
-                >
-                  delete
-                </button>
-              </td>
-            </tr>
+            <TaskItem
+              key={task.id}
+              task={task}
+              handleRefresh={handleRefresh}
+            ></TaskItem>
           ))}
-          {
-            tasks.length === 0 && !loading && <tr><td colSpan={5}>No tasks found</td></tr>
-          }
+          {tasks.length === 0 && !loading && (
+            <tr>
+              <td colSpan={5}>No tasks found. Try logging in again.</td>
+            </tr>
+          )}
         </tbody>
       </table>
       <button onClick={handleRefresh} className={styless.button}>
         {loading ? 'Loading...' : 'Refresh'}
       </button>
     </div>
+  );
+}
+
+function TaskItem({
+  task,
+  handleRefresh,
+}: {
+  task: Task;
+  handleRefresh: Function;
+}) {
+  const [title, setTitle] = useState<string>(task.title);
+  const [description, setDescription] = useState<string>(task.description);
+  const [points, setPoints] = useState<number>(task.points);
+
+  let edited=title!==task.title || description!==task.description || points!==task.points;
+
+  function handleDeleteTask(id: number) {
+    //todo make fetch request
+    handleRefresh();
+  }
+
+  function handleUpdateTask(){
+    //todo make fetch request
+    handleRefresh();
+  }
+
+  return (
+    <tr key={task.id}>
+      <td className={styles.td}>{task.id}</td>
+      <input
+        className={styles.td}
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      ></input>
+      <td className={styles.td}>
+        <textarea
+          className={styles.textarea}
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
+      </td>
+      <input
+        className={styles.td}
+        type='number'
+        value={points}
+        onChange={(e) => setPoints(Number(e.target.value))}
+      ></input>
+      <td className={styles.td}>
+        <button
+          className={styless.button}
+          onClick={() => handleDeleteTask(task.id)}
+          disabled={!edited}
+        >
+          update
+        </button>
+      </td>
+      <td className={styles.td}>
+        <button
+          className={styless.button}
+          onClick={() => handleDeleteTask(task.id)}
+        >
+          delete
+        </button>
+      </td>
+    </tr>
   );
 }
 
