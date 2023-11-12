7ba412467d9ad4a148eff318f4e077490bc83d5d
Anoushka Jha <95405559+BabyElias@users.noreply.github.com>
2023-11-12 13:00:22 +0530

    Update landingPage.tsx

--- src/pages/landingPage.tsx
+++ src/pages/landingPage.tsx
@@ -2,7 +2,7 @@ import FAQ from '@/components/faq';
 import OurReach from '../components/ourReach';
 import Landingpage from '@/components/landingPage';
 import Testimonials from '@/components/testimonials';
-import AboutTechnex from '@/components/aboutTechnex';
+import AboutTechnex from '@/components/abouttechnex';
 import ScrollIcon from '@/components/scrollicon';
 import ContactUs from '@/components/contactUs';
 
