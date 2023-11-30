b43369949a9b6bbd422ee6f3ee37cc3480521e46
swetagit186 <sweta.kumari.eee22@itbhu.ac.in>
2023-11-30 23:07:57 +0530

    commits done

---
+++ src/pages/faqPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '@/components/navbar'
+import FAQ from '@/components/faq'
+
+const faqPage = () => {
+  return (
+    <div>
+        <Navbar></Navbar>
+        <FAQ></FAQ>
+    </div>
+  )
+}
+
+export default faqPage
