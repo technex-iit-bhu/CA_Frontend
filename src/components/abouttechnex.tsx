acb44a66b99300c208d8fb3ce8db9fbd2ee08225
V Abhijith <119759593+VAbhijith2003github@users.noreply.github.com>
2023-12-13 19:12:44 +0530

    fix: change text in aboutus and whyca sections

--- src/components/abouttechnex.tsx
+++ src/components/abouttechnex.tsx
@@ -20,31 +20,33 @@ const AboutTechnex: FC = () => {
           <div className='flex'>
             <div className='left-column'>
               <p className='mr-3 border-b-2 border-[#A81F25] p-3 pr-5 pt-10 tracking-wide lg:w-[850px]'>
-                TECHNEX&apos;23, IIT BHU is one of the largest and oldest
-                college fests in India.&nbsp; Embodying the true spirit of
-                youth,{' '}
+                Technex'23, hosted by the prestigious Indian Institute of
+                Technology (IIT) BHU, stands as a venerable beacon among India's
+                college festivals.{' '}
                 <span className='text-[#A81F25]'>
-                  Technex provides a platform for young talent from all over
-                  India to showcase their varied skills.
-                </span>
-                &nbsp; Technex is an annual technical festival organized by the
-                students of Indian Institute of Technology (IIT) BHU,&nbsp;
-                Varanasi.<br></br>
-                The festival provides a platform for students to showcase their
-                technical and creative abilities through{' '}
+                  Revered as one of the largest and oldest gatherings of its
+                  kind, this annual technical extravaganza epitomizes the
+                  vibrant spirit of youth
+                </span>{' '}
+                . Serving as a distinguished platform for emerging talents
+                across the nation, Technex unfurls a tapestry of skills,
+                seamlessly weaving together the technical and creative prowess
+                of students.<br></br>
+                This illustrious festival, meticulously organized by the
+                students of IIT BHU, Varanasi, transcends conventional
+                boundaries with its myriad offerings.&nbsp;
                 <span className='text-[#A81F25]'>
-                  various competitions, workshops, guest lectures, and
-                  exhibitions
-                </span>
-                . It attracts a large number of students from all over the
-                country and provides a unique opportunity for them to showcase
-                their skills and network with their peers and industry
-                professionals.{' '}
-                <span className='text-[#A81F25]'>
-                  Technex is a celebration of innovation and technology, and a
-                  platform for students to learn, grow, and excel in their field
-                  of interest.
-                </span>
+                  From compelling competitions and enlightening workshops to
+                  captivating guest lectures and insightful exhibitions, Technex
+                  embodies a celebration of innovation and technology
+                </span>{' '}
+                . Drawing participants from every corner of the country, it
+                provides a distinctive opportunity for students to not only
+                showcase their abilities but also engage in meaningful
+                networking with peers and industry professionals. Technex
+                emerges as a crucible for personal and professional growth,
+                where students can learn, evolve, and truly excel in their
+                chosen fields{' '}
               </p>
             </div>
           </div>
