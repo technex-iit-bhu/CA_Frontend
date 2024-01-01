603dda64edaafca6103114c5a5fcfe57fd80a31a
Varun-Kolanu <kolanuvarun739@gmail.com>
2024-01-01 20:49:14 +0530

    incentives in completed

--- src/components/dashboardCard.tsx
+++ src/components/dashboardCard.tsx
@@ -220,7 +220,7 @@ const Cards: React.FC<Props> = ({
             as={Button}
             rightIcon={<ChevronDownIcon />}
             className='absolute bottom-0 left-0'
-          > {activeTab === 'live' ? 'Incentives' : 'Comments'} </MenuButton>
+          > {activeTab === 'live' || activeTab === 'completed' ? 'Incentives' : 'Comments'} </MenuButton>
           <MenuList className='z-10'>
             <MenuItem
               minH='48px'
@@ -228,7 +228,7 @@ const Cards: React.FC<Props> = ({
               className='menuItem flex-shrink-1 flex bg-[#A81F25] p-[10px] md:w-1/2 lg:w-1/3'
             >
               {
-                activeTab === 'live' ? 
+                activeTab === 'live' || activeTab === 'completed' ? 
                 (incentives ? incentives : 'No Incentives') :
                 (comment ? comment : 'No Comments') 
               }
