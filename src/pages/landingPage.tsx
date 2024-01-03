e610655edbe016fed8556bfc750cf34c0aead4e0
Vaibhav Tripathi <96713176+vaibhav-1508@users.noreply.github.com>
2024-01-03 19:35:09 +0530

    Update landingPage.tsx

--- src/pages/landingPage.tsx
+++ src/pages/landingPage.tsx
@@ -3,10 +3,12 @@ import Landingpage from '@/components/landingPage';
 import Testimonials from '@/components/testimonials';
 import AboutTechnex from '@/components/abouttechnex';
 import ScrollIcon from '@/components/scrollicon';
+import Navbar from '../components/navbar';
 
 export default function LandingPage() {
   return (
     <>
+      <Navbar />
       <Landingpage />
       <ScrollIcon />
       <AboutTechnex />
